c031779d22db388cc44460977c2f2c261ca91292
BeratKuzu55 <75533278+BeratKuzu55@users.noreply.github.com>
2020-12-09 21:59:24 +0300

    List box

---
+++ NetFramework.Sıra19.D9.ListBoxİnceleme/Ürün.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sıra19.D7.ComboBoxİnceleme
+{
+    public class Ürün
+    {
+
+
+        public int id { get; set; }
+        public string urunAdı { get; set; }
+        public string ürünResim { get; set; }
+        public string urunKategori { get; set; }
+        public int stokAdet{ get; set; }
+        public string yazar { get; set; }
+        public string açıklama { get; set; }
+
+
+        public override string ToString()
+        {
+            return urunAdı;
+        }
+
+
+
+
+    }
+}

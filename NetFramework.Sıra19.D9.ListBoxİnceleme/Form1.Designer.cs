68eda5c0ee2957331101b242b13dd2f002ce8153
BeratKuzu55 <75533278+BeratKuzu55@users.noreply.github.com>
2020-12-09 21:22:24 +0300

    basit hesap makinesi

---
+++ NetFramework.Sıra19.D9.ListBoxİnceleme/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace NetFramework.Sıra19.D9.ListBoxİnceleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxÜrünler = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TextBoxÜrünAçıklama = new System.Windows.Forms.RichTextBox();
+            this.textBoxStokAdedi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxYazar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxKategori = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxÜrünAdı = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxÜrünler
+            // 
+            this.listBoxÜrünler.FormattingEnabled = true;
+            this.listBoxÜrünler.Location = new System.Drawing.Point(0, 0);
+            this.listBoxÜrünler.Name = "listBoxÜrünler";
+            this.listBoxÜrünler.Size = new System.Drawing.Size(256, 238);
+            this.listBoxÜrünler.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TextBoxÜrünAçıklama);
+            this.groupBox1.Controls.Add(this.textBoxStokAdedi);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxYazar);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxKategori);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxÜrünAdı);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(262, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 238);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seçilen Ürün Detay Bilgisi";
+            // 
+            // TextBoxÜrünAçıklama
+            // 
+            this.TextBoxÜrünAçıklama.Location = new System.Drawing.Point(0, 119);
+            this.TextBoxÜrünAçıklama.Name = "TextBoxÜrünAçıklama";
+            this.TextBoxÜrünAçıklama.Size = new System.Drawing.Size(440, 119);
+            this.TextBoxÜrünAçıklama.TabIndex = 3;
+            this.TextBoxÜrünAçıklama.Text = "";
+            // 
+            // textBoxStokAdedi
+            // 
+            this.textBoxStokAdedi.Location = new System.Drawing.Point(178, 64);
+            this.textBoxStokAdedi.Name = "textBoxStokAdedi";
+            this.textBoxStokAdedi.Size = new System.Drawing.Size(263, 20);
+            this.textBoxStokAdedi.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Stok Adedi";
+            // 
+            // textBoxYazar
+            // 
+            this.textBoxYazar.Location = new System.Drawing.Point(177, 93);
+            this.textBoxYazar.Name = "textBoxYazar";
+            this.textBoxYazar.Size = new System.Drawing.Size(263, 20);
+            this.textBoxYazar.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Yazar";
+            // 
+            // textBoxKategori
+            // 
+            this.textBoxKategori.Location = new System.Drawing.Point(177, 42);
+            this.textBoxKategori.Name = "textBoxKategori";
+            this.textBoxKategori.Size = new System.Drawing.Size(263, 20);
+            this.textBoxKategori.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kategori";
+            // 
+            // textBoxÜrünAdı
+            // 
+            this.textBoxÜrünAdı.Location = new System.Drawing.Point(177, 16);
+            this.textBoxÜrünAdı.Name = "textBoxÜrünAdı";
+            this.textBoxÜrünAdı.Size = new System.Drawing.Size(263, 20);
+            this.textBoxÜrünAdı.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ürün Adı";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 97);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 246);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBoxÜrünler);
+            this.Name = "Form1";
+            this.Text = "ListBox İnceleme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxÜrünler;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox TextBoxÜrünAçıklama;
+        private System.Windows.Forms.TextBox textBoxStokAdedi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxYazar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxKategori;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxÜrünAdı;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ NetFramework.Sıra19.D9.ListBoxİnceleme/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetFramework.Sıra19.D9.ListBoxİnceleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxDoldurV1();
+        }
+
+        void listBoxDoldurV1()
+        {
+            foreach (var item in database.ürünTablo)
+            {
+                listBoxÜrünler.Items.Add(item);/*
+                item.add objeden değer kabul ettiği için objelerim ilgili listbox a eklendi
+                her eklenen obje ürün nesnesi ama objeye dönüştürüldü
+                 */
+            }
+           
+        }
+
+    }
+}
